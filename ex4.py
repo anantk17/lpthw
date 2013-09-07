2337ef25192ee9f784b33e11c787aed7258ec5bf
anant <anantk17@ymail.com>
2013-09-07 23:01:13 +0530

    Added comment lines

--- ex4.py
+++ ex4.py
@@ -1,10 +1,19 @@
+#assign cars with the total number of available cars
 cars = 100
+#assign cars with the number of people that can fit inside the car
 space_in_a_car = 4.0
+#assign drivers with the number of drivers available
 drivers = 30
+#assign passengers with the total number of passengers
 passengers  = 90
+#assign cars_not_driven with the number of unmanned cars
 cars_not_driven = cars - drivers
+#assign cars_driven with the number of manned cars
 cars_driven = drivers
+#assign carpool_capacity with the total number of people who can be carpooled
 carpool_capacity = cars_driven * space_in_a_car
+
+#assign average_passengers_per_car with the actual number of people in one car
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
